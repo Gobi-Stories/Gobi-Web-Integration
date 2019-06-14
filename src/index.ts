1219d4b4771e100808ce038f162325e58c66f775
Ola Brunborg Vikholt <ovikholt@gmail.com>
2019-06-14 11:22:49 +0200

    Add bluebird for Promises on IE (increases file size ouch)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./styles.scss";
 import { default as _Player } from "@/Player";
 import { default as _Bubbles } from "@/Bubbles";
+import "./bluebird.min";
 
 export const Player = _Player;
 export const Bubbles = _Bubbles;
